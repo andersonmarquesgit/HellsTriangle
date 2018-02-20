a16a9510fdac15216a569b5b08a1bb4bd085f236
Anderson Marques <andersonmarques.ci@gmail.com>
2018-02-19 22:27:42 -0300

    Adicionado parse para entrada padrão

--- src/main/java/com/hells/Main.java
+++ src/main/java/com/hells/Main.java
@@ -1,9 +1,24 @@
 package com.hells;
 
+import java.util.Scanner;
+
+import com.hells.calc.TriangleCalc;
+import com.hells.geometric.Triangle;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		Scanner in = new Scanner(System.in);
+		String inputArray = in.nextLine();
+		
+		if(!inputArray.isEmpty()) {
+			Integer[][] t = TriangleCalc.getElements(inputArray);
+			
+			Triangle triangle = new Triangle(t);
+			System.out.println(TriangleCalc.getMaximumTotal(triangle));
+		}else {
+			System.out.println(0);
+		}
 	}
-
 }
--- src/main/java/com/hells/calc/TriangleCalc.java
+++ src/main/java/com/hells/calc/TriangleCalc.java
@@ -1,10 +1,17 @@
 package com.hells.calc;
 
-import com.hells.model.Triangle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import com.hells.geometric.Triangle;
 
 public abstract class TriangleCalc {
 
-	public int getMaximumTotal(Triangle triangle) {
+	public static int getMaximumTotal(Triangle triangle) {
 		int maxTotal = triangle.getTop();
 		int col = 0;
 		
@@ -15,5 +22,38 @@ public abstract class TriangleCalc {
 		}
 		
 		return maxTotal;
+	}
+	
+	public static Integer[][] getElements(String inputArray) {
+		String triangleRows = parseInput(inputArray);
+		return parseTriangleRows(triangleRows);
+	}
+	
+	protected static String parseInput(String array) {
+		String regex = "(\\[|\\{)(.+)(\\]|\\})";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(array);
+		if (matcher.find()) {
+			return matcher.group(2);
+		}
+		return "";
+	}
+
+	protected static Integer[][] parseTriangleRows(String rows) {
+		List<Integer[]> rowsList = new ArrayList<>();
+		String regex = "((\\[|\\{)(((\\d+),?)+)(\\]|\\}))";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(rows);
+		while (matcher.find()) {
+			String group = matcher.group(3);
+			Integer[] row = Arrays.stream(Stream.of(group.split(",")).mapToInt(Integer::parseInt).toArray()).boxed()
+					.toArray(Integer[]::new);
+			rowsList.add(row);
+		}
+		Integer[][] triangleArray = new Integer[rowsList.size()][];
+		for (int i = 0; i < triangleArray.length; i++) {
+			triangleArray[i] = rowsList.get(i);
+		}
+		return triangleArray;
 	}
 }
